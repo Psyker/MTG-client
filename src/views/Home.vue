82386b66794b336a6883121bee313ce748c7f7f2
Psyker <bourgoin.theo1@gmail.com>
2018-08-06 00:22:34 +0200

    Create project, add apollo, first graphql query and home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <label>
+      <input type="text" v-model="name">
+    </label>
+    <!--<ApolloQuery :debounce="400" :variables="{name}" :query="require('../graphql/queries/CardsQuery.graphql')">-->
+      <!--<template slot-scope="{result:  {loading, error, data}}">-->
+        <div v-if="$apollo.loading">Loading ...</div>
+       <div v-else-if="cards" v-for="card in cards" :key="card.id">
+         <img :src="card.imageUrl" alt="">
+         <div v-for="color in card.colors" :key="color">{{color}}</div>
+       </div>
+      <!--</template>-->
+    <!--</ApolloQuery>-->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    data() {
+      return {
+        name: ""
+      }
+    },
+    apollo: {
+      cards: {
+        query: require('../graphql/queries/CardsQuery.graphql'),
+        variables() {
+          return {name: this.name}
+        },
+        debounce: 400
+      }
+    }
   }
-}
 </script>
